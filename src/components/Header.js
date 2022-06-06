416722ed9066d79d5b678517b9cbd57b9c426bbd
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-06 15:04:09 +0530

    Building a very basic react static app to look back at the react fundamentals.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "../styles.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h2>
+        <span>🌐 </span>
+        My Travel Journal
+      </h2>
+    </div>
+  );
+}
