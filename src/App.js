4ac28f44bded500021c4626c2fe76735191f5628
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-06 17:52:28 +0530

    basic app setup is now ready with messed up styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./styles.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import cardData from "./cardData";
+
+const cardElements = cardData.map((card) => {
+  return <Card card={card} />;
+});
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card />
+      {cardElements}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,30 @@
 import React from "react";
 
-export default function Card() {
-  return <div>Card</div>;
+export default function Card(props) {
+  console.log(props);
+  return (
+    <div className="card">
+      <div>
+        <img src={props.card.imageUrl} />
+      </div>
+      <div className="card-container">
+        <div>
+          <h4>{props.card.location}</h4>
+          <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8">View on map</a>
+        </div>
+        <h3>Mount Fuji</h3>
+        <div date>
+          <p>12 Jan, 2021</p>
+          <p>24 Jan, 2021</p>
+        </div>
+        <div>
+          <small>
+            Mount Fuji is the tallest mountain in Japan, standing at 3,776
+            meters (12,380 feet). Mount Fuji is the single most popular tourist
+            site in Japan, for both Japanese and foreign tourists.
+          </small>
+        </div>
+      </div>
+    </div>
+  );
 }
